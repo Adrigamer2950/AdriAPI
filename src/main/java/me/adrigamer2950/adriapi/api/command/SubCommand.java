f1c4a0051d7edd706812b098b10f915a68214692
Adrigamer2950 <adrigamer2950@gmail.com>
2023-10-19 16:17:29 +0200

    Deleted tab complete method to SubCommand

--- src/main/java/me/adrigamer2950/adriapi/api/command/SubCommand.java
+++ src/main/java/me/adrigamer2950/adriapi/api/command/SubCommand.java
@@ -1,5 +1,7 @@
 package me.adrigamer2950.adriapi.api.command;
 
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -11,5 +13,10 @@ public abstract class SubCommand extends Command {
 
     public SubCommand(Command parent, String name, @Nullable List<String> aliases) {
         super(parent.getPlugin(), name, aliases);
+    }
+
+    @Nullable
+    public final List<String> onTabComplete(@NotNull final CommandSender sender, @NotNull org.bukkit.command.Command command, @NotNull String alias, @NotNull String[] args) {
+        return null;
     }
 }
