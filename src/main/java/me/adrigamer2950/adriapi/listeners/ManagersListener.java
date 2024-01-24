914a4713178c11306af5caf942e8df7cbfe021d6
Adrigamer2950 <adrigamer2950@gmail.com>
2024-01-24 18:32:08 +0100

    Scrapped Database System

--- src/main/java/me/adrigamer2950/adriapi/listeners/ManagersListener.java
+++ src/main/java/me/adrigamer2950/adriapi/listeners/ManagersListener.java
@@ -2,7 +2,6 @@ package me.adrigamer2950.adriapi.listeners;
 
 import me.adrigamer2950.adriapi.api.command.manager.CommandManager;
 import me.adrigamer2950.adriapi.api.config.manager.ConfigManager;
-import me.adrigamer2950.adriapi.api.database.manager.DatabaseManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.server.PluginDisableEvent;
 import org.jetbrains.annotations.ApiStatus;
@@ -17,12 +16,6 @@ public class ManagersListener implements org.bukkit.event.Listener {
             CommandManager.COMMAND_MANAGERS.remove(CommandManager.getManager(e.getPlugin()));
             CommandManager.LOGGER.log(String.format(
                     String.format("Command Manager for %s v%s has been successfully unloaded", e.getPlugin().getName(), e.getPlugin().getDescription().getVersion())
-            ));
-        }
-        if (DatabaseManager.getManager(e.getPlugin()) != null) {
-            DatabaseManager.DATABASE_MANAGERS.remove(DatabaseManager.getManager(e.getPlugin()));
-            DatabaseManager.LOGGER.log(String.format(
-                    String.format("Database Manager for %s v%s has been successfully unloaded", e.getPlugin().getName(), e.getPlugin().getDescription().getVersion())
             ));
         }
         if (ConfigManager.getManager(e.getPlugin()) != null) {
