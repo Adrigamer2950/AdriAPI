82c1bf09694f7fbffe4256c7947d1a38eac738fe
Adrigamer2950 <adrigamer2950@gmail.com>
2024-07-30 17:32:25 +0200

    Removed config system

--- src/main/java/me/adrigamer2950/adriapi/listeners/ManagersListener.java
+++ src/main/java/me/adrigamer2950/adriapi/listeners/ManagersListener.java
@@ -1,7 +1,6 @@
 package me.adrigamer2950.adriapi.listeners;
 
 import me.adrigamer2950.adriapi.api.command.manager.CommandManager;
-import me.adrigamer2950.adriapi.api.config.manager.ConfigManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.server.PluginDisableEvent;
 import org.jetbrains.annotations.ApiStatus;
@@ -16,12 +15,6 @@ public class ManagersListener implements org.bukkit.event.Listener {
             CommandManager.COMMAND_MANAGERS.remove(CommandManager.getManager(e.getPlugin()));
             CommandManager.LOGGER.log(String.format(
                     String.format("Command Manager for %s v%s has been successfully unloaded", e.getPlugin().getName(), e.getPlugin().getDescription().getVersion())
-            ));
-        }
-        if (ConfigManager.getManager(e.getPlugin()) != null) {
-            ConfigManager.CONFIG_MANAGERS.remove(ConfigManager.getManager(e.getPlugin()));
-            ConfigManager.LOGGER.log(String.format(
-                    String.format("Config Manager for %s v%s has been successfully unloaded", e.getPlugin().getName(), e.getPlugin().getDescription().getVersion())
             ));
         }
     }
