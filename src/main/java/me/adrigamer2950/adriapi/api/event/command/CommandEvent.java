39be01bc7afbe57139207207cf42b4b9bbfff7cd
Adrigamer2950 <adrigamer2950@gmail.com>
2024-12-22 01:11:45 +0100

    feat: Make CommandEvent abstract

--- src/main/java/me/adrigamer2950/adriapi/api/event/command/CommandEvent.java
+++ src/main/java/me/adrigamer2950/adriapi/api/event/command/CommandEvent.java
@@ -4,21 +4,13 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import me.adrigamer2950.adriapi.api.command.Command;
 import org.bukkit.event.Event;
-import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * @see Command
  */
 @Getter
 @RequiredArgsConstructor
-public class CommandEvent extends Event {
-    private static final HandlerList handlers = new HandlerList();
-
-    @Override
-    public @NotNull HandlerList getHandlers() {
-        return handlers;
-    }
+public abstract class CommandEvent extends Event {
 
     private final Command<?> command;
 }
--- src/main/java/me/adrigamer2950/adriapi/api/event/command/CommandLoadedEvent.java
+++ src/main/java/me/adrigamer2950/adriapi/api/event/command/CommandLoadedEvent.java
@@ -2,7 +2,9 @@ package me.adrigamer2950.adriapi.api.event.command;
 
 import lombok.Getter;
 import me.adrigamer2950.adriapi.api.command.Command;
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * This event will be triggered when a {@link Command} is successfully loaded
@@ -11,6 +13,18 @@ import org.bukkit.plugin.Plugin;
  */
 @Getter
 public class CommandLoadedEvent extends CommandEvent {
+
+    private static final HandlerList handlers = new HandlerList();
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return handlers;
+    }
+
+    @SuppressWarnings("unused")
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
 
     private final Plugin loader;
 
