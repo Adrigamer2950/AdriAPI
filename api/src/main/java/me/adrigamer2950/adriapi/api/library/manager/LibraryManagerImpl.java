339912e19ffc66aeff5ef912cf73bad61506a236
Adrigamer2950 <adrigamer2950@gmail.com>
2025-03-16 00:13:23 +0100

    feat: Centralize all downloaded libraries into AdriAPI folder

--- api/src/main/java/me/adrigamer2950/adriapi/api/library/manager/LibraryManagerImpl.java
+++ api/src/main/java/me/adrigamer2950/adriapi/api/library/manager/LibraryManagerImpl.java
@@ -1,26 +1,38 @@
 package me.adrigamer2950.adriapi.api.library.manager;
 
-import net.byteflux.libby.BukkitLibraryManager;
 import net.byteflux.libby.Library;
+import net.byteflux.libby.classloader.URLClassLoaderHelper;
+import net.byteflux.libby.logging.adapters.JDKLogAdapter;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.ApiStatus;
 
+import java.io.File;
+import java.net.URLClassLoader;
+import java.nio.file.Path;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 @ApiStatus.Internal
-public class LibraryManagerImpl extends BukkitLibraryManager implements LibraryManager {
+public class LibraryManagerImpl extends net.byteflux.libby.LibraryManager implements LibraryManager {
+
+    private final URLClassLoaderHelper classLoader;
 
     protected final Plugin plugin;
     protected final Set<Library> libraries = new HashSet<>();
 
     public LibraryManagerImpl(Plugin plugin) {
-        super(plugin);
+        super(new JDKLogAdapter(plugin.getLogger()), new File(Bukkit.getPluginsFolder(), "AdriAPI").toPath(), "lib");
+        this.classLoader = new URLClassLoaderHelper((URLClassLoader) plugin.getClass().getClassLoader(), this);
 
         this.plugin = plugin;
 
         this.addMavenCentral();
+    }
+
+    protected void addToClasspath(Path file) {
+        this.classLoader.addToClasspath(file);
     }
 
     @Override
