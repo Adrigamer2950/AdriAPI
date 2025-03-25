cad430be7bc30ab75f4bda032254f37a840e3a2d
Adrigamer2950 <adrigamer2950@gmail.com>
2025-03-25 16:33:26 +0100

    Migrate ScheduledTask to Kotlin

--- api/folia/src/main/kotlin/me/adrigamer2950/adriapi/api/scheduler/ScheduledTask.kt
+++ api/folia/src/main/kotlin/me/adrigamer2950/adriapi/api/scheduler/ScheduledTask.kt
@@ -1,33 +1,27 @@
-package me.adrigamer2950.adriapi.api.scheduler;
+package me.adrigamer2950.adriapi.api.scheduler
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.plugin.Plugin
+import org.bukkit.scheduler.BukkitTask
+import io.papermc.paper.threadedregions.scheduler.ScheduledTask as FoliaScheduledTask
 
-@RequiredArgsConstructor
-public final class ScheduledTask {
+open class ScheduledTask(val task: Any, val owner: Plugin) {
 
-    @Getter
-    private boolean cancelled = false;
-    private final Object task;
-
-    @Getter
-    private final Plugin owner;
+    var cancelled: Boolean = false
+        private set
 
     /**
      * Cancels this task
-     * @throws IllegalStateException If the task is already cancelled
      */
-    public void cancel() {
+    @Throws(IllegalStateException::class)
+    fun cancel() {
         if (cancelled) {
-            throw new IllegalStateException("Task is already cancelled");
+            throw IllegalStateException ("Task is already cancelled");
         }
 
-        if (task instanceof BukkitTask) {
-            ((BukkitTask) task).cancel();
-        } else if (task instanceof io.papermc.paper.threadedregions.scheduler.ScheduledTask) {
-            ((io.papermc.paper.threadedregions.scheduler.ScheduledTask) task).cancel();
+        if (task is BukkitTask) {
+            task.cancel();
+        } else if (task is FoliaScheduledTask) {
+            task.cancel();
         }
 
         cancelled = true;
