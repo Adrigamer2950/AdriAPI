1b2fefd75f9abb0dd1dccc5c7fc4332899ae2e81
Adrigamer2950 <adrigamer2950@gmail.com>
2025-04-09 19:25:06 +0200

    Handle inventory interactions only if the listener matches the inventory's associated plugin

--- api/src/main/kotlin/me/adrigamer2950/adriapi/api/listeners/InventoriesListener.kt
+++ api/src/main/kotlin/me/adrigamer2950/adriapi/api/listeners/InventoriesListener.kt
@@ -1,16 +1,19 @@
 package me.adrigamer2950.adriapi.api.listeners
 
+import me.adrigamer2950.adriapi.api.APIPlugin
 import me.adrigamer2950.adriapi.api.inventory.Inventory
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.inventory.InventoryCloseEvent
 
-class InventoriesListener : Listener {
+class InventoriesListener(val plugin: APIPlugin) : Listener {
 
     @EventHandler
     fun onInventoryClick(event: InventoryClickEvent) {
         val inventory = event.inventory.holder as? Inventory ?: return
+
+        if (inventory.plugin != plugin) return
 
         inventory.onInventoryClick(event)
     }
@@ -18,6 +21,8 @@ class InventoriesListener : Listener {
     @EventHandler
     fun onInventoryClose(event: InventoryCloseEvent) {
         val inventory = event.inventory.holder as? Inventory ?: return
+
+        if (inventory.plugin != plugin) return
 
         inventory.onInventoryClose(event)
     }
