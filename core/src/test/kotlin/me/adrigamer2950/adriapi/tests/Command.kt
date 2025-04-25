6f796beed299f9505b5da3124b5b39e824f2afa0
Adrigamer2950 <adrigamer2950@gmail.com>
2025-04-25 16:21:19 +0200

    Move test command registration to a BeforeAll function

--- core/src/test/kotlin/me/adrigamer2950/adriapi/tests/Command.kt
+++ core/src/test/kotlin/me/adrigamer2950/adriapi/tests/Command.kt
@@ -2,15 +2,21 @@ package me.adrigamer2950.adriapi.tests
 
 import me.adrigamer2950.adriapi.common.ExampleCommand
 import me.adrigamer2950.adriapi.platform.AbstractTestPlatform
-import kotlin.test.Test
-import kotlin.test.assertNotNull
+import org.junit.jupiter.api.BeforeAll
+import kotlin.test.*
 
 class Command : AbstractTestPlatform() {
 
+    companion object {
+        @JvmStatic
+        @BeforeAll
+        fun `Register command`() {
+            plugin.commandManager.registerCommand(ExampleCommand(plugin))
+        }
+    }
+
     @Test
     fun `Command is registered`() {
-        plugin.commandManager.registerCommand(ExampleCommand(plugin))
-
         assertNotNull(plugin.commandManager.getCommandOrNull("example"), "Command 'example' should be registered")
     }
 }
