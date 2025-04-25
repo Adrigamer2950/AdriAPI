6cf732df805333902f98604c0433eecb6a0bcdd8
Adrigamer2950 <adrigamer2950@gmail.com>
2025-04-25 04:29:56 +0200

    Start and stop server before and after all tests

--- core/src/test/kotlin/me/adrigamer2950/adriapi/platform/AbstractTestPlatform.kt
+++ core/src/test/kotlin/me/adrigamer2950/adriapi/platform/AbstractTestPlatform.kt
@@ -1,25 +1,29 @@
 package me.adrigamer2950.adriapi.platform
 
 import me.adrigamer2950.adriapi.TestPlugin
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.mockbukkit.mockbukkit.MockBukkit
 import org.mockbukkit.mockbukkit.ServerMock
-import kotlin.test.AfterTest
-import kotlin.test.BeforeTest
 
 abstract class AbstractTestPlatform {
 
-    lateinit var server: ServerMock
-    lateinit var plugin: TestPlugin
+    companion object {
+        lateinit var server: ServerMock
+        lateinit var plugin: TestPlugin
 
-    @BeforeTest
-    open fun startServer() {
-        server = MockBukkit.mock()
+        @JvmStatic
+        @BeforeAll
+        fun startServer() {
+            server = MockBukkit.mock()
 
-        plugin = MockBukkit.load(TestPlugin::class.java)
-    }
+            plugin = MockBukkit.load(TestPlugin::class.java)
+        }
 
-    @AfterTest
-    open fun stopServer() {
-        MockBukkit.unmock()
+        @JvmStatic
+        @AfterAll
+        fun stopServer() {
+            MockBukkit.unmock()
+        }
     }
 }
