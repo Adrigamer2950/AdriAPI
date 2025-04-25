388ebc60391e9a688b95c477e97a52a30103a06d
Adrigamer2950 <adrigamer2950@gmail.com>
2025-04-25 15:51:06 +0200

    Improve test execution

--- core/src/test/kotlin/me/adrigamer2950/adriapi/platform/AbstractTestPlatform.kt
+++ core/src/test/kotlin/me/adrigamer2950/adriapi/platform/AbstractTestPlatform.kt
@@ -1,8 +1,10 @@
 package me.adrigamer2950.adriapi.platform
 
 import me.adrigamer2950.adriapi.TestPlugin
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.BeforeAll
+import me.adrigamer2950.adriapi.platform.AbstractTestPlatform.Companion.plugin
+import me.adrigamer2950.adriapi.platform.AbstractTestPlatform.Companion.server
+import org.junit.platform.launcher.TestExecutionListener
+import org.junit.platform.launcher.TestPlan
 import org.mockbukkit.mockbukkit.MockBukkit
 import org.mockbukkit.mockbukkit.ServerMock
 
@@ -11,19 +13,18 @@ abstract class AbstractTestPlatform {
     companion object {
         lateinit var server: ServerMock
         lateinit var plugin: TestPlugin
+    }
+}
 
-        @JvmStatic
-        @BeforeAll
-        fun startServer() {
-            server = MockBukkit.mock()
+class GlobalTestListener : TestExecutionListener {
 
-            plugin = MockBukkit.load(TestPlugin::class.java)
-        }
+    override fun testPlanExecutionStarted(testPlan: TestPlan?) {
+        server = MockBukkit.mock()
 
-        @JvmStatic
-        @AfterAll
-        fun stopServer() {
-            MockBukkit.unmock()
-        }
+        plugin = MockBukkit.load(TestPlugin::class.java)
+    }
+
+    override fun testPlanExecutionFinished(testPlan: TestPlan?) {
+        MockBukkit.unmock()
     }
 }
