3889a9b359e47b4bb96418757997ea0f9812f2f9
Adrigamer2950 <adrigamer2950@gmail.com>
2025-04-25 16:44:40 +0200

    Move GlobalTestListener to a separate file

--- core/src/test/kotlin/me/adrigamer2950/adriapi/platform/AbstractTestPlatform.kt
+++ core/src/test/kotlin/me/adrigamer2950/adriapi/platform/AbstractTestPlatform.kt
@@ -1,11 +1,6 @@
 package me.adrigamer2950.adriapi.platform
 
 import me.adrigamer2950.adriapi.TestPlugin
-import me.adrigamer2950.adriapi.platform.AbstractTestPlatform.Companion.plugin
-import me.adrigamer2950.adriapi.platform.AbstractTestPlatform.Companion.server
-import org.junit.platform.launcher.TestExecutionListener
-import org.junit.platform.launcher.TestPlan
-import org.mockbukkit.mockbukkit.MockBukkit
 import org.mockbukkit.mockbukkit.ServerMock
 
 abstract class AbstractTestPlatform {
@@ -13,18 +8,5 @@ abstract class AbstractTestPlatform {
     companion object {
         lateinit var server: ServerMock
         lateinit var plugin: TestPlugin
-    }
-}
-
-class GlobalTestListener : TestExecutionListener {
-
-    override fun testPlanExecutionStarted(testPlan: TestPlan?) {
-        server = MockBukkit.mock()
-
-        plugin = MockBukkit.load(TestPlugin::class.java)
-    }
-
-    override fun testPlanExecutionFinished(testPlan: TestPlan?) {
-        MockBukkit.unmock()
     }
 }
---
+++ core/src/test/kotlin/me/adrigamer2950/adriapi/platform/GlobalTestListener.kt
@@ -0,0 +1,21 @@
+package me.adrigamer2950.adriapi.platform
+
+import me.adrigamer2950.adriapi.TestPlugin
+import me.adrigamer2950.adriapi.platform.AbstractTestPlatform.Companion.plugin
+import me.adrigamer2950.adriapi.platform.AbstractTestPlatform.Companion.server
+import org.junit.platform.launcher.TestExecutionListener
+import org.junit.platform.launcher.TestPlan
+import org.mockbukkit.mockbukkit.MockBukkit
+
+class GlobalTestListener : TestExecutionListener {
+
+    override fun testPlanExecutionStarted(testPlan: TestPlan?) {
+        server = MockBukkit.mock()
+
+        plugin = MockBukkit.load(TestPlugin::class.java)
+    }
+
+    override fun testPlanExecutionFinished(testPlan: TestPlan?) {
+        MockBukkit.unmock()
+    }
+}
