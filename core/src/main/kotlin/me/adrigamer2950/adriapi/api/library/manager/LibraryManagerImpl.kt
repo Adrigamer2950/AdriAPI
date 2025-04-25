eee3bfff4d2deae3f1bd1def0aaf7677dc8af833
Adrigamer2950 <adrigamer2950@gmail.com>
2025-04-25 03:54:49 +0200

    Switched from libby-bukkit to libby-standalone

--- core/src/main/kotlin/me/adrigamer2950/adriapi/api/library/manager/LibraryManagerImpl.kt
+++ core/src/main/kotlin/me/adrigamer2950/adriapi/api/library/manager/LibraryManagerImpl.kt
@@ -1,34 +1,24 @@
 package me.adrigamer2950.adriapi.api.library.manager
 
 import com.alessiodp.libby.Library
-import com.alessiodp.libby.classloader.URLClassLoaderHelper
+import com.alessiodp.libby.StandaloneLibraryManager
 import com.alessiodp.libby.logging.adapters.JDKLogAdapter
 import org.bukkit.Bukkit
 import org.bukkit.plugin.Plugin
 import org.jetbrains.annotations.ApiStatus.Internal
 import java.io.File
-import java.net.URLClassLoader
-import java.nio.file.Path
-import com.alessiodp.libby.LibraryManager as LibbyLibraryManager
 
 @Internal
-class LibraryManagerImpl internal constructor(override val plugin: Plugin, folderName: String = plugin.name) : LibbyLibraryManager(
+class LibraryManagerImpl internal constructor(override val plugin: Plugin, folderName: String = plugin.name) : StandaloneLibraryManager(
     JDKLogAdapter(plugin.logger), File(
         Bukkit.getPluginsFolder(), folderName
     ).toPath(), "lib"
 ), LibraryManager {
 
-    private val classLoader: URLClassLoaderHelper =
-        URLClassLoaderHelper(plugin.javaClass.getClassLoader() as URLClassLoader, this)
-
     override val libraries: MutableSet<Library> = HashSet()
 
     init {
         this.addMavenCentral()
-    }
-
-    override fun addToClasspath(file: Path) {
-        this.classLoader.addToClasspath(file)
     }
 
     override fun addLibrary(library: Library) {
