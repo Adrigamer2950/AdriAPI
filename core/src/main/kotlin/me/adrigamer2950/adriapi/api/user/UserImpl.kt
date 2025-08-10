2d0388d9afec4c854ffe822e8670556da6254819
Adrigamer2950 <adrigamer2950@gmail.com>
2025-08-10 05:55:11 +0200

    Simplify UserImpl#sendMessage

--- core/src/main/kotlin/me/adrigamer2950/adriapi/api/user/UserImpl.kt
+++ core/src/main/kotlin/me/adrigamer2950/adriapi/api/user/UserImpl.kt
@@ -2,7 +2,6 @@ package me.adrigamer2950.adriapi.api.user
 
 import me.adrigamer2950.adriapi.api.colors.Colors
 import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
 import org.bukkit.command.CommandSender
 import org.bukkit.command.ConsoleCommandSender
 import org.bukkit.entity.Player
@@ -33,10 +32,7 @@ class UserImpl internal constructor(override val bukkitSender: CommandSender) : 
 
     @Override
     override fun sendMessage(vararg components: Component) {
-        if (isPlayer())
-            components.forEach { bukkitSender.sendMessage(it) }
-        else
-            this.sendMessage(*components.map { LegacyComponentSerializer.legacyAmpersand().serialize(it) }.toTypedArray())
+        components.forEach { bukkitSender.sendMessage(it) }
     }
 
     override fun hasPermission(permission: String): Boolean {
