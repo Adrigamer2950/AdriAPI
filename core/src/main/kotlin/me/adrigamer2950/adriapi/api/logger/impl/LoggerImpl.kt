a29391c514ec1755e9d51597233224ec51495244
Adri <business@devadri.es>
2025-08-31 16:35:18 +0200

    Simplify LoggerImpl

--- core/src/main/kotlin/me/adrigamer2950/adriapi/api/logger/impl/LoggerImpl.kt
+++ core/src/main/kotlin/me/adrigamer2950/adriapi/api/logger/impl/LoggerImpl.kt
@@ -1,15 +1,12 @@
 package me.adrigamer2950.adriapi.api.logger.impl
 
 import me.adrigamer2950.adriapi.api.APIPlugin
-import me.adrigamer2950.adriapi.api.colors.Colors
 import me.adrigamer2950.adriapi.api.logger.Logger
 import me.adrigamer2950.adriapi.api.logger.builder.LogBuilder
 import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 import org.bukkit.Bukkit
 import java.util.logging.Level
-import java.util.logging.LogRecord
 import java.util.logging.Logger as JavaLogger
 
 @Suppress("unused")
@@ -26,7 +23,7 @@ class LoggerImpl(name: String, parent: JavaLogger? = Bukkit.getServer().logger) 
 
     private fun parseMessage(msg: Any): String {
         return if (msg is Component) {
-            LegacyComponentSerializer.legacyAmpersand().serialize(msg)
+            PlainTextComponentSerializer.plainText().serialize(msg)
         } else {
             msg.toString()
         }
@@ -44,11 +41,5 @@ class LoggerImpl(name: String, parent: JavaLogger? = Bukkit.getServer().logger) 
         val message = parseMessage(log.message)
 
         super.log(log.level, "$prefix$message", log.throwable)
-    }
-
-    override fun log(record: LogRecord) {
-        record.message = PlainTextComponentSerializer.plainText().serialize(Colors.legacyToComponent(record.message))
-
-        super.log(record)
     }
 }
