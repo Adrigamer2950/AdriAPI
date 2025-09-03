880cb41b64c50be13fe75bca9b33a271d5fad3ae
Adri <business@devadri.es>
2025-09-03 21:53:14 +0200

    Add Coordinates#fromSlot

--- core/src/main/kotlin/me/devadri/obsidian/menu/coords/Coordinates.kt
+++ core/src/main/kotlin/me/devadri/obsidian/menu/coords/Coordinates.kt
@@ -10,4 +10,12 @@ class Coordinates(raw: Int, column: Int) {
     fun toSlot(): Int {
         return column * raw
     }
+
+    companion object {
+        fun fromSlot(slot: Int): Coordinates {
+            val row = slot / 9
+            val column = slot % 9
+            return Coordinates(row, column)
+        }
+    }
 }
