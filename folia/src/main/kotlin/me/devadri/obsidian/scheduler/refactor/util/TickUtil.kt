4eebc2d8712087da41a8b7c8e23b9a3f9a32b0fc
Adri <business@devadri.es>
2025-09-05 15:02:23 +0200

    Add TickUtil to transform TimeUnit into ticks

---
+++ folia/src/main/kotlin/me/devadri/obsidian/scheduler/refactor/util/TickUtil.kt
@@ -0,0 +1,17 @@
+package me.devadri.obsidian.scheduler.refactor.util
+
+import java.util.concurrent.TimeUnit
+
+object TickUtil {
+
+    @JvmStatic
+    fun timeToTicks(unit: TimeUnit, time: Long): Long = when (unit) {
+        TimeUnit.NANOSECONDS -> time / 50_000
+        TimeUnit.MICROSECONDS -> time / 50
+        TimeUnit.MILLISECONDS -> time / 50
+        TimeUnit.SECONDS -> time * 20
+        TimeUnit.MINUTES -> time * 20 * 60
+        TimeUnit.HOURS -> time * 20 * 60 * 60
+        TimeUnit.DAYS -> time * 20 * 60 * 60 * 24
+    }
+}
